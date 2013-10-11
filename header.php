18b312f207271971ce3755a895e047ac51cdeb5d
mytory <mytory@gmail.com>
2013-10-11 11:22:59 +0900

    IE 호환성모드 강제 끄기

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
   <meta name="viewport" content="width=device-width" />
   <title><?php wp_title( '|', true, 'right' ); ?><?=esc_attr(get_bloginfo('name'))?></title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
   <?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
   <!--[if lt IE 9]>
